4b8c41884ff8b7d17ad2d3d3a8a4a9c2d1cc2fd3
Eivind Dogger <eivind.dogger@gmail.com>
2022-05-03 23:21:54 +0200

    Create model analysis script with size, fps, map, etc

---
+++ Project/model_analysis.py
@@ -0,0 +1,64 @@
+from tabnanny import check
+import time
+import click
+import torch
+import tops
+import json
+from ssd import utils
+from pathlib import Path
+from tops.config import instantiate
+from tops.checkpointer import load_checkpoint
+
+
+@torch.no_grad()
+def evaluation(cfg, N_images: int):
+    model =instantiate(cfg.model)
+    model.eval()
+    model = tops.to_cuda(model)
+    ckpt = load_checkpoint(cfg.output_dir.joinpath("checkpoints"), map_location=tops.get_device())
+    model.load_state_dict(ckpt["model"])
+    dataloader_val = instantiate(cfg.data_val.dataloader)
+    batch = next(iter(dataloader_val))
+    gpu_transform = instantiate(cfg.data_val.gpu_transform)
+    batch = tops.to_cuda(batch)
+    batch = gpu_transform(batch)
+    images = batch["image"]
+    imshape = list(images.shape[2:])
+
+    print("Number of parameters in model:", sum(p.numel() for p in model.parameters()))
+    print("Number of trainable parameters in model:", sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    # warmup
+    print("Checking runtime for image shape:", imshape)
+    for i in range(10):
+        model(images)
+    start_time = time.time()
+    for i in range(N_images):
+        outputs = model(images)
+    total_time = time.time() - start_time
+    print("Runtime for image shape:", imshape)
+    print("Total runtime:", total_time)
+    print("Number of images:", N_images)
+    print("FPS:", N_images / total_time)
+
+    file = open(cfg.output_dir.joinpath("logs/scalars.json"), 'r')
+    mAP = []
+    for line in file:
+        data = json.loads(line)
+        if 'metrics/mAP' in data:
+            mAP.append(data['metrics/mAP'])
+    mAP = torch.tensor(mAP)
+    print("Max mAP:", float(torch.max(mAP)))
+    print("Final mAP:", float(mAP[-1]))
+    
+
+@click.command()
+@click.argument("config_path", type=click.Path(exists=True, dir_okay=False, path_type=Path))
+@click.option("-n", "--n-images", default=100, type=int)
+def main(config_path: Path, n_images: int):
+    cfg = utils.load_config(config_path)
+    evaluation(cfg, n_images)
+
+
+if __name__ == '__main__':
+    main()
