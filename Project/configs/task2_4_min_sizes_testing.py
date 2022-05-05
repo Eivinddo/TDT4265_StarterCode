4386527d6567794e122d525edd5777552a10413e
Eivind Dogger <eivind.dogger@gmail.com>
2022-05-05 16:13:36 +0200

    Add test config file for min sixes and ar

--- Project/configs/task2_4_min_sizes_testing.py
+++ Project/configs/task2_4_min_sizes_testing.py
@@ -1,4 +1,3 @@
-from math import gamma
 from ssd.modeling import backbones, SSDFocalLoss, AnchorBoxes, RetinaNet
 from tops.config import LazyCall as L
 from ssd.modeling.backbones import FPN
@@ -18,9 +17,10 @@ anchors = L(AnchorBoxes)(
     feature_sizes=[[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
     # Strides is the number of pixels (in image space) between each spatial position in the feature map
     strides=[[4, 4], [8, 8], [16, 16], [32, 32], [64, 64], [128, 128]],
-    #min_sizes=[[16, 16], [32, 32], [48, 48], [64, 64], [86, 86], [128, 128], [128, 400]],
-    min_sizes=[[14, 10], [26, 24], [40, 40], [60, 60], [84, 88], [120, 136], [134, 420]],
-    aspect_ratios=[[3, 4], [2, 4], [2, 3], [2, 3], [2, 3], [2, 3]],
+    min_sizes=[[16, 16], [32, 32], [48, 48], [64, 64], [86, 86], [128, 128], [128, 400]],
+    #min_sizes=[[14, 14], [30, 26], [48, 48], [64, 64], [86, 86], [126, 128], [128, 400]],
+    #min_sizes=[[14, 14], [30, 30], [48, 48], [64, 64], [86, 86], [128, 128], [128, 400]],
+    aspect_ratios=[[1.5, 4], [1.5, 4], [1.5, 4], [1.5, 4], [1.5, 4], [1.5, 4]],
     image_shape="${train.imshape}",
     scale_center_variance=0.1,
     scale_size_variance=0.2
